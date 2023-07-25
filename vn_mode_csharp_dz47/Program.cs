ad982a4b47cc58629420fac9dfdd0333bc3dae67
VITALII NAGIBIN <98809212+vn-mode@users.noreply.github.com>
2023-07-25 19:57:54 +0300

    ijunior_csharp_dz47

--- vn_mode_csharp_dz47/Program.cs
+++ vn_mode_csharp_dz47/Program.cs
@@ -1,20 +1,48 @@
 ﻿using System;
 using System.Linq;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите название страны один:");
+        string countryOneName = Console.ReadLine();
+
+        Console.WriteLine("Введите количество солдат в взводе страны один:");
+        int squadOneSize = int.Parse(Console.ReadLine());
+
+        Console.WriteLine();
+
+        Console.WriteLine("Введите название страны два:");
+        string countryTwoName = Console.ReadLine();
+
+        Console.WriteLine("Введите количество солдат в взводе страны два:");
+        int squadTwoSize = int.Parse(Console.ReadLine());
+
+        Console.WriteLine();
+
+        BattleSimulator.SimulateBattle(countryOneName, countryTwoName, squadOneSize, squadTwoSize);
+
+        Console.ReadLine();
+    }
+}
+
 class Soldier
 {
     public string Name { get; private set; }
     public int Health { get; private set; }
+    private int damage;
 
-    public Soldier(string name, int health)
+    public Soldier(string name, int health, int damage)
     {
         Name = name;
         Health = health;
+        this.damage = damage;
     }
 
     public void Attack(Soldier enemy)
     {
-        enemy.TakeDamage(1);
+        enemy.TakeDamage(damage);
     }
 
     private void TakeDamage(int damage)
@@ -44,36 +72,41 @@ class BattleSimulator
     private const string WinnerMessage = " победил!";
     private const string DrawMessage = "Бой окончился вничью.";
 
+    private static Random random = new Random();
+
     public static void SimulateBattle(string countryOneName, string countryTwoName, int squadOneSize, int squadTwoSize)
     {
         Console.WriteLine("Начинается бой между " + countryOneName + " и " + countryTwoName + "!");
         Console.WriteLine();
 
-        Soldier[] squadOneSoldiers = GenerateSoldiers(squadOneSize);
-        Soldier[] squadTwoSoldiers = GenerateSoldiers(squadTwoSize);
-
-        Squad squadOne = new Squad(squadOneSoldiers);
-        Squad squadTwo = new Squad(squadTwoSoldiers);
+        Squad squadOne = new Squad(GenerateSoldiers(squadOneSize));
+        Squad squadTwo = new Squad(GenerateSoldiers(squadTwoSize));
 
         while (squadOne.Soldiers.Length > 0 && squadTwo.Soldiers.Length > 0)
         {
-            Soldier attacker = squadOne.Soldiers[0];
-            Soldier defender = squadTwo.Soldiers[new Random().Next(0, squadTwo.Soldiers.Length)];
-
-            Console.WriteLine(attacker.Name + " атакует " + defender.Name + "!");
-            attacker.Attack(defender);
-
-            if (defender.Health <= 0)
+            for (int i = 0; i < squadOne.Soldiers.Length; i++)
             {
-                Console.WriteLine(defender.Name + SoldierEliminatedMessage);
-                squadTwo.RemoveSoldier(defender);
+                if (squadTwo.Soldiers.Length == 0)
+                    break;
+
+                Soldier attacker = squadOne.Soldiers[i];
+                Soldier defender = squadTwo.Soldiers[random.Next(0, squadTwo.Soldiers.Length)];
+
+                Console.WriteLine(attacker.Name + " атакует " + defender.Name + "!");
+                attacker.Attack(defender);
+
+                if (defender.Health <= 0)
+                {
+                    Console.WriteLine(defender.Name + SoldierEliminatedMessage);
+                    squadTwo.RemoveSoldier(defender);
+                }
+
+                Console.WriteLine();
             }
 
             Squad temp = squadOne;
             squadOne = squadTwo;
             squadTwo = temp;
-
-            Console.WriteLine();
         }
 
         if (squadOne.Soldiers.Length > 0)
@@ -93,14 +126,14 @@ class BattleSimulator
     private static Soldier[] GenerateSoldiers(int squadSize)
     {
         Soldier[] soldiers = new Soldier[squadSize];
-        Random random = new Random();
 
         for (int i = 0; i < squadSize; i++)
         {
             string soldierName = GenerateRandomName();
             int soldierHealth = random.Next(10, 20);
+            int soldierDamage = random.Next(2, 5);
 
-            soldiers[i] = new Soldier(soldierName, soldierHealth);
+            soldiers[i] = new Soldier(soldierName, soldierHealth, soldierDamage);
         }
 
         return soldiers;
@@ -111,36 +144,9 @@ class BattleSimulator
         string[] names = { "Александр", "Дмитрий", "Иван", "Максим", "Артем", "Андрей", "Николай", "Сергей", "Михаил", "Алексей" };
         string[] surnames = { "Смирнов", "Иванов", "Кузнецов", "Соколов", "Попов", "Лебедев", "Козлов", "Новиков", "Морозов", "Петров" };
 
-        Random random = new Random();
         string name = names[random.Next(names.Length)];
         string surname = surnames[random.Next(surnames.Length)];
 
         return name + " " + surname;
-    }
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Введите название страны один:");
-        string countryOneName = Console.ReadLine();
-
-        Console.WriteLine("Введите количество солдат в взводе страны один:");
-        int squadOneSize = int.Parse(Console.ReadLine());
-
-        Console.WriteLine();
-
-        Console.WriteLine("Введите название страны два:");
-        string countryTwoName = Console.ReadLine();
-
-        Console.WriteLine("Введите количество солдат в взводе страны два:");
-        int squadTwoSize = int.Parse(Console.ReadLine());
-
-        Console.WriteLine();
-
-        BattleSimulator.SimulateBattle(countryOneName, countryTwoName, squadOneSize, squadTwoSize);
-
-        Console.ReadLine();
     }
 }
